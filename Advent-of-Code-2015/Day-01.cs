620bce3ae938513701b5e77ee63bc9e479882f1a
Dellson <pwdela@gmail.com>
2017-12-09 10:52:54 +0100

    Zadanie #1 ukońcozne (2*)

---
+++ Advent-of-Code-2015/Day-01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2015
+{
+    class Day_01
+    {
+        private static string input = File.ReadAllLines(Program.InputFolderPath + "Day-01-input.txt")[0];
+
+        public static void BothStars()
+        {
+            int floor = 0;
+            int position = -1;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (input[i] == '(')
+                    floor++;
+                if (input[i] == ')')
+                {
+                    floor--;
+                    if (position == -1 && floor < 0)
+                        position = i + 1;
+                }
+            }
+
+            Console.WriteLine(floor);
+            Console.WriteLine(position);
+        }
+    }
+}
--- Advent-of-Code-2015/Program.cs
+++ Advent-of-Code-2015/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Console;
 
 namespace Advent_of_Code_2015
 {
     class Program
     {
+        public static string InputFolderPath { get; private set; }
+
+        static Program()
+        {
+            InputFolderPath = System.IO.Directory.GetCurrentDirectory();
+            InputFolderPath = System.IO.Path.Combine(InputFolderPath, "..\\..\\");
+        }
+
         static void Main(string[] args)
         {
+            Day_01.BothStars();
+
+            ReadKey();
         }
     }
 }
