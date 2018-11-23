91196b2e936546311766346fe797c7b44f95a211
Paweł Dela <pwdela@gmail.com>
2018-11-23 14:29:48 +0100

    Zadanie #8 (1*)

---
+++ Advent-of-Code-2015/Day-08.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Advent_of_Code_2015
+{
+    class Day_08
+    {
+        private static readonly string[] input = System.IO.File.ReadAllLines(Program.InputFolderPath + "Day-08-input.txt");
+
+        public static void RemoveSpecialChars()
+        {
+            int counter = 0;
+            int counter2 = 0;
+            foreach (var line in input)
+            {
+                counter2 += line.Length;
+                for (int i = 1; i < line.Length-1; i++)
+                {
+                    if (line[i] == '\\')
+                    {
+                        i++;
+                        if (line[i] == 'x')
+                        {
+                            i += 2;
+                        }
+                    }
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter2 - counter);
+        }
+    }
+}
--- Advent-of-Code-2015/Program.cs
+++ Advent-of-Code-2015/Program.cs
@@ -21,7 +21,8 @@ namespace Advent_of_Code_2015
             //Day_05.BothStars();
             //Day_06.BothStars();
             //Day_07.BothStars();
-            Day_09.BothStars();
+            Day_08.RemoveSpecialChars();
+            //Day_09.BothStars();
             ReadKey();
         }
     }
