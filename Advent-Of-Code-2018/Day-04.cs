82638c5b5aa6a81a4dc2e46b54b954b31ab023de
Dellson <pwdela@gmail.com>
2018-12-05 09:08:40 +0100

    AoC 2018 Day #4 (2*)

--- Advent-Of-Code-2018/Day-04.cs
+++ Advent-Of-Code-2018/Day-04.cs
@@ -9,88 +9,108 @@ namespace Advent_of_Code_2018
     class Day_04
     {
         private static string[] _input = System.IO.File.ReadAllLines(Program.InputFolderPath + "Day-04-input.txt");
-        private static Dictionary<int, Guard> guards = new Dictionary<int, Guard>();
+        private static Dictionary<int, Dictionary<int, int>> guards = new Dictionary<int, Dictionary<int, int>>();
 
         static Day_04()
         {
-            var set = _input.ToList();
-            for (int i = 0; i < _input.Length; i++)
+            var sortedInput = _input.ToList();
+            sortedInput.Sort();
+
+            int currentId = 0;
+            int aslept = 0;
+
+            foreach (var line in sortedInput)
             {
-                set.Sort((x, y) => string.Compare(x, y));
-                //var id = Convert.ToInt32(Regex.Matches(_input[i], @"\d+").Value);
-            }
-
-
-            /*var myList = new List<Guard>();
-            for (int i = 0; i < _input.Length; i++)
-            {
-                // 1518-10-08 00:16
-                DateTime myDate = DateTime.ParseExact(_input[i].Substring(1,16), "yyyy-MM-dd HH:mm",
-                                       System.Globalization.CultureInfo.InvariantCulture);
-
-                Guard g = new Guard();
-                g.timestamp = myDate;
-                g.desc = _input[i].Substring(18);
-                myList.Add(g);
-                //Console.WriteLine("DEBUG");
-            }*/
-                //_input[i] = Convert.ToInt32(Regex.Match(rawInput[i], @"-?\d+").Value);
-
-
-            //myList.Sort((x, y) => DateTime.Compare(x.timestamp, y.timestamp));
-
-            var id = 0;
-
-            foreach (var line in set)
-            {
-                var digits = Regex.Matches(line, @"\d+");
-                var currentid = Convert.ToInt32(digits[digits.Count - 1].Value);
-                //Console.WriteLine(currentid);
-                if (line.Contains("Guard") && !guards.ContainsKey(id) && digits.Count == 6)
+                if (line.Contains("Guard"))
                 {
-                    // 6
-                    //Console.WriteLine("   " + digits.Count);
-                    id = currentid;
-                    guards.Add(id, new Guard(id));
-                    
+                    currentId = Convert.ToInt32(Regex.Match(line, @"#\d+").Value.Substring(1));
+
+                    if (!guards.ContainsKey(currentId))
+                        guards.Add(currentId, new Dictionary<int, int>());
+                    continue;
                 }
-                
-                DateTime myDate = DateTime.ParseExact(line.Substring(1,16), "yyyy-MM-dd HH:mm",
-                                       System.Globalization.CultureInfo.InvariantCulture);
-
-                guards[id].desc.Add(new Tuple<DateTime, string>(myDate, line.Substring(19)));
-            }
-
-            foreach (var guard in guards)
-	        {
-                Console.WriteLine(guard.Key);
-
-                foreach (var item in guard.Value.desc)
+                if (line.Contains("falls asleep"))
                 {
-                    //Console.WriteLine(item.Item1 + " " + item.Item2);
+                    aslept = Convert.ToInt32(line.Substring(15, 2));
                 }
-	        }
+                if (line.Contains("wakes up"))
+                {
+                    var curTime = Convert.ToInt32(line.Substring(15, 2));
+                    var diff = curTime - aslept;
+
+                    for (int i = aslept; i < curTime; i++)
+                    {
+                        if (!guards[currentId].ContainsKey(i))
+                            guards[currentId].Add(i, 1);
+                        else
+                            guards[currentId][i]++;
+                    }
+                }
+            }
         }
 
         public static void Puzzle()
         {
-            //int sum = 0;
-            //var collection = new HashSet<int>();
+            int minute = 0;
+            int repetitions = 0;
+            int max = 0;
+            int gu = 0;
+            
+            foreach (var guard in guards)
+            {
+                if (guard.Value.Values.Count == 0)
+                    continue;
+                int mostOftenMinuteCount = guard.Value.Values.Max();
+                int mostOftenMinute = guard.Value.Where(v => v.Value == mostOftenMinuteCount).First().Key;
+                Console.WriteLine("DEBUG " + mostOftenMinute);
 
-            WriteLine();
+                if (mostOftenMinuteCount > repetitions)
+                {
+                    repetitions = mostOftenMinuteCount;
+                    minute = mostOftenMinute;
+                    gu = guard.Key;
+                }
+                    
+
+                /*minute = guard.Value.Where(v => v.Value == maxVal).First().Key;
+                if (minute > max)
+                {
+                    max = minute;
+                    gu = guard.Key;
+                }*/
+                    
+            }
+            Console.WriteLine(minute);
+            Console.WriteLine(gu);
+            Console.WriteLine(minute * gu);
+            // 4972 too low
+            
         }
 
-        class Guard
+        public static void Puzzle2()
         {
-            public readonly int Id;
-            //public readonly DateTime Timestamp;
-            //public readonly string FullDescription;
-            //public Dictionary<string> desc;
-            public List<Tuple<DateTime, string>> desc = new List<Tuple<DateTime, string>>();
+            int minute = 0;
+            int max = 0;
+            int gu = 0;
 
-            public Guard(int Id){
-                Id = Id;
-}
+            foreach (var guard in guards)
+            {
+                //Console.WriteLine(guard.Key);
+                int cur = 0;
+
+                foreach (var item in guard.Value)
+                {
+                    cur += item.Value;
+                }
+                if (cur > max)
+                {
+                    max = cur;
+                    int maxVal = guard.Value.Values.Max();
+                    minute = guard.Value.Where(v => v.Value == maxVal).First().Key;
+                    gu = guard.Key;
+                }
+            }
+            Console.WriteLine(minute * gu);
         }
     }
 }
