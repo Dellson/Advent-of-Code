26fa2a8dc19e064d6ea8053073082e1f754786f7
Paweł Dela <pwdela@gmail.com>
2018-12-18 15:34:36 +0100

    AoC 2018 d07 (2*)

--- Advent-of-Code-2018/Day-07.cs
+++ Advent-of-Code-2018/Day-07.cs
@@ -11,52 +11,15 @@ namespace Advent_of_Code_2018
         private static string[] _rawInput = System.IO.File.ReadAllLines(Program.InputFolderPath + "Day-07-input.txt");
         private static Dictionary<string, List<string>> _steps = new Dictionary<string, List<string>>();
 
-        static Day_07()
-        {
-            foreach (var line in _rawInput)
-            {
-                var words = Regex.Matches(line, @"\w+"); // 1, 7
-
-                if (!_steps.ContainsKey(words[7].Value))
-                    _steps.Add(words[7].Value, new List<string> { words[1].Value });
-                else
-                    _steps[words[7].Value].Add(words[1].Value);
-
-                if (!_steps.ContainsKey(words[1].Value))
-                    _steps.Add(words[1].Value, new List<string>());
-            }
-        }
-
         public static void Puzzle()
         {
-            List<string> result = PuzzleOne();
-            int time = 0;
-            List<int> workers = new List<int> { 0, 0 };
-
-            for (int i = 0; i < result.Count; i++)
-            {
-                var workerIndex = workers.FindIndex(w => w == 0);
-
-                if (workerIndex != -1)
-                    workers[workerIndex] = Convert.ToInt32(result[i][0]) - 64;
-                else
-                    i--;
-
-                time++;
-
-                for (int j = 0; j < workers.Count; j++)
-                {
-                    if (workers[j] != 0)
-                        workers[j]--;
-                }
-            }
-
-            WriteLine(time + workers.Max());
+            PuzzleOne();
+            PuzzleTwo();
         }
-        // , 0, 0, 0
 
-        private static List<string> PuzzleOne()
+        private static void PuzzleOne()
         {
+            Initialize();
             List<string> result = new List<string>();
             int len = _steps.Count;
             var startSteps = _steps.Where(v => v.Value.Count == 0).OrderBy(e => e.Key);
@@ -76,12 +39,69 @@ namespace Advent_of_Code_2018
                 _steps.Remove(next);
             }
 
-            foreach (var line in result)
-                Write(line);
+            result.ForEach(s => Write(s));
+            WriteLine();
+        }
 
-            return result;
+        private static void PuzzleTwo()
+        {
+            Initialize();
+            List<string> result = new List<string>();
+            int len = _steps.Count;
+            var startSteps = _steps.Where(v => v.Value.Count == 0).OrderBy(e => e.Key);
+            int time = 0;
+            List<Worker> workers = new List<Worker>{ new Worker(), new Worker(), new Worker(), new Worker(), new Worker() };
+
+            foreach (var task in startSteps)
+                TryAssigningTaskToWorker(task.Key);
+
+            for (; result.Count < len; time++)
+            {
+                var next = _steps.Where(step => step.Value.All(
+                    s => result.Contains(s))).ToList();
+
+                for (int i = 0; i < next.Count; i++)
+                    TryAssigningTaskToWorker(next[i].Key);
+
+                for (int j = 0; j < workers.Count; j++)
+                    if (workers[j].Time > 0 && --workers[j].Time == 0)
+                        result.Add(workers[j].CurrentTask);
+            }
+
+            WriteLine(time);
+
+            void TryAssigningTaskToWorker(string task)
+            {
+                var workerIndex = workers.FindIndex(w => w.Time == 0);
+
+                if (workerIndex != -1)
+                {
+                    _steps.Remove(task);
+                    workers[workerIndex].CurrentTask = task;
+                    workers[workerIndex].Time = Convert.ToInt32(task[0]) - 4;
+                }
+            }
+        }
+        private static void Initialize()
+        {
+            foreach (var line in _rawInput)
+            {
+                var words = Regex.Matches(line, @"\w+");
+
+                if (!_steps.ContainsKey(words[7].Value))
+                    _steps.Add(words[7].Value, new List<string> { words[1].Value });
+                else
+                    _steps[words[7].Value].Add(words[1].Value);
+
+                if (!_steps.ContainsKey(words[1].Value))
+                    _steps.Add(words[1].Value, new List<string>());
+            }
+        }
+
+        class Worker
+        {
+            public string CurrentTask = string.Empty;
+            public int Time = 0;
         }
     }
 }
-
-// A = 65
