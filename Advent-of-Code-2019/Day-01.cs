6fa8b98d2877717e15955b8f26bfe6fbf2ef6530
Paweł Dela <pwdela@gmail.com>
2019-12-01 16:25:23 +0100

    Started AoC 2019. Puzzle one completed (2*)

---
+++ Advent-of-Code-2019/Day-01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_Code_2019
+{
+    class Day_01
+    {
+        public static void Puzzle()
+        {
+            var listOfMasses = 
+                File.ReadAllLines(Program.InputFolderPath + "Day-01-input.txt")
+                .ToList()
+                .ConvertAll(Convert.ToInt32);
+
+            int puzzleOneAnswer = listOfMasses.Select(mass => PuzzleOneCalculator(mass)).Sum();
+            int puzzleTwoAnswer = listOfMasses.Select(mass => PuzzleTwoCalculator(mass)).Sum();
+
+            Console.WriteLine($"Puzzle one answer: {puzzleOneAnswer}");
+            Console.WriteLine($"Puzzle two answer: {puzzleTwoAnswer}");
+        }
+
+        private static int PuzzleOneCalculator(int mass) => (mass / 3) - 2;
+
+        private static int PuzzleTwoCalculator(int mass)
+        {
+            int fuelMass = PuzzleOneCalculator(mass);
+
+            if (fuelMass > 0)
+                return fuelMass + PuzzleTwoCalculator(fuelMass);
+            else
+                return 0;
+        }
+    }
+}
---
+++ Advent-of-Code-2019/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using static System.Console;
+
+namespace Advent_of_Code_2019
+{
+    class Program
+    {
+        public static string InputFolderPath { get; private set; }
+
+        static Program()
+        {
+            InputFolderPath = Directory.GetCurrentDirectory();
+            InputFolderPath = Path.Combine(InputFolderPath, "..\\..\\InputFiles\\");
+        }
+
+        static void Main(string[] args)
+        {
+            Day_01.Puzzle();
+            //Day_02.Puzzle();
+            //Day_03.Puzzle();
+            //Day_04.Puzzle();
+            //Day_05.Puzzle();
+            //Day_07.Puzzle();
+            //Day_08.Puzzle();
+            ReadKey();
+        }
+    }
+}
