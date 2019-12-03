884b38fabe192eb803f108540d69107b602fa6eb
Paweł Dela <pwdela@gmail.com>
2019-12-03 20:28:44 +0100

    Day 4 preparations.

---
+++ Advent-of-Code-2019/Day-04.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2019
+{
+    class Day_04
+    {
+        public static void Puzzle()
+        {
+            var input = File.ReadAllLines(Program.InputFolderPath + "Day-04-input.txt");
+
+            var puzzleOneAnswer = "PUZZLE ONE ANSWER";
+            var puzzleTwoAnswer = "PUZZLE TWO ANSWER";
+
+            Console.WriteLine($"Puzzle one answer: {puzzleOneAnswer}");
+            Console.WriteLine($"Puzzle two answer: {puzzleTwoAnswer}");
+        }
+    }
+}
