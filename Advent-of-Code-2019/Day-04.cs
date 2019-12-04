c19cd82268bea0fa8558a508ef4323b032a75beb
Paweł Dela <pwdela@gmail.com>
2019-12-04 16:44:10 +0100

    Day 4 puzzle one completed (1*)

--- Advent-of-Code-2019/Day-04.cs
+++ Advent-of-Code-2019/Day-04.cs
@@ -10,7 +10,33 @@ namespace Advent_of_Code_2019
     {
         public static void Puzzle()
         {
-            var input = File.ReadAllLines(Program.InputFolderPath + "Day-04-input.txt");
+            int min = 123257;
+            int max = 647015;
+            int counter = 0;
+
+            int[] minT = new int[] { 1, 2, 3, 2, 5, 7 };
+            int[] maxT = new int[] { 6, 4, 7, 0, 1, 5 };
+
+            for (int i = 123257; i <= 647015; i++)
+            {
+                string converted = i.ToString();
+                var unsorted = converted.Select(digit => int.Parse(digit.ToString())).ToList();
+                var sorted = new List<int>(unsorted);
+                sorted.Sort();
+
+                if (unsorted.SequenceEqual(sorted)
+                    && sorted.Distinct().Count() != sorted.Count())
+                {
+                    //unsorted.ForEach(e => Console.Write(e + ", "));
+                    //Console.Write("\t\t");
+                    //sorted.ForEach(e => Console.Write(e + ", "));
+                    //Console.WriteLine();
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(647015 - 123257);
+            Console.WriteLine(counter);
 
             var puzzleOneAnswer = "PUZZLE ONE ANSWER";
             var puzzleTwoAnswer = "PUZZLE TWO ANSWER";
