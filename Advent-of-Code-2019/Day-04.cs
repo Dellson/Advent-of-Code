0bf5cf44dd9de2ed7da7f786d44166e5710c03a6
Paweł Dela <pwdela@gmail.com>
2019-12-04 16:52:44 +0100

    Day 4 part II completed (2*)

--- Advent-of-Code-2019/Day-04.cs
+++ Advent-of-Code-2019/Day-04.cs
@@ -24,8 +24,7 @@ namespace Advent_of_Code_2019
                 var sorted = new List<int>(unsorted);
                 sorted.Sort();
 
-                if (unsorted.SequenceEqual(sorted)
-                    && sorted.Distinct().Count() != sorted.Count())
+                if (Analyze(unsorted, sorted))
                 {
                     //unsorted.ForEach(e => Console.Write(e + ", "));
                     //Console.Write("\t\t");
@@ -35,14 +34,37 @@ namespace Advent_of_Code_2019
                 }
             }
 
-            Console.WriteLine(647015 - 123257);
+            //Console.WriteLine(647015 - 123257);
             Console.WriteLine(counter);
 
-            var puzzleOneAnswer = "PUZZLE ONE ANSWER";
-            var puzzleTwoAnswer = "PUZZLE TWO ANSWER";
+            var puzzleOneAnswer = counter;
+            var puzzleTwoAnswer = "Null pointer exception: answer not found";
 
             Console.WriteLine($"Puzzle one answer: {puzzleOneAnswer}");
             Console.WriteLine($"Puzzle two answer: {puzzleTwoAnswer}");
+        }
+
+        private static bool Analyze(List<int> unsorted, List<int> sorted)
+        {
+            bool areAllConditionsMet = false;
+
+            if (unsorted.SequenceEqual(sorted)
+                    && sorted.Distinct().Count() != sorted.Count())
+            {
+                List<int> digitsCount = new List<int>();
+
+                foreach (var digit in sorted)
+                {
+                    digitsCount.Add(sorted.FindAll(d => d == digit).Count);
+                }
+
+                if (digitsCount.Contains(2))
+                {
+                    areAllConditionsMet = true;
+                }
+            }
+
+            return areAllConditionsMet;
         }
     }
 }
