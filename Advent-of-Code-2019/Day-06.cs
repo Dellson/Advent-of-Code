d391aa3465b90d55355816ff043c5e17edade881
Paweł Dela <pwdela@gmail.com>
2019-12-06 17:25:44 +0100

    Day 6 refactoring

--- Advent-of-Code-2019/Day-06.cs
+++ Advent-of-Code-2019/Day-06.cs
@@ -2,44 +2,36 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Advent_of_Code_2019
 {
     class Day_06
     {
-        static Dictionary<string, List<string>> objectsDescendants = new Dictionary<string, List<string>>();
+        static Dictionary<string, List<string>> objectsHierarchy = new Dictionary<string, List<string>>();
         static Dictionary<string, List<string>> objectsFullDescendants = new Dictionary<string, List<string>>();
         private static int count = 0;
 
         public static void Puzzle()
         {
-            var inputList =
+            var mapData =
                 File.ReadAllLines(Program.InputFolderPath + "Day-06-input.txt");
 
-            // initial fill: save map data to dicts
-            foreach (string objectToAnalyze in inputList)
+            foreach (string orbitData in mapData)
             {
-                (string gravitationalPullSource, string orbitingObject) = 
-                    (objectToAnalyze.Split(')')[0], objectToAnalyze.Split(')')[1]);
+                (string ascendant, string descendant) = 
+                    (orbitData.Split(')')[0], orbitData.Split(')')[1]);
 
-                if (!objectsDescendants.ContainsKey(gravitationalPullSource))
-                    objectsDescendants.Add(gravitationalPullSource, new List<string>());
+                if (!objectsHierarchy.ContainsKey(ascendant))
+                    objectsHierarchy.Add(ascendant, new List<string>());
 
-                objectsDescendants[gravitationalPullSource].Add(orbitingObject);
+                objectsHierarchy[ascendant].Add(descendant);
             }
 
-            // part one
-            foreach (var gravitationalPullSource in objectsDescendants)
-                objectsFullDescendants.Add(gravitationalPullSource.Key, FindDescendants(gravitationalPullSource.Value));
+            foreach (var descendant in objectsHierarchy)
+                objectsFullDescendants.Add(descendant.Key, FindDescendants(descendant.Value));
 
             Console.WriteLine($"Puzzle one answer: {count}");
-
-            // part two
-            var r = FindFirstCommonAscendant("YOU", "SAN");
-            Console.WriteLine($"Puzzle two answer: {r}");
-            //inputInstructions.CopyTo(copiedInstructions, 0);
-            //Console.WriteLine($"Puzzle two answer {IntcodeComputer.CalculateOutput(copiedInstructions, 5)}");
+            Console.WriteLine($"Puzzle two answer: {GetMinimumOrbitalTransfersForTwoObjects("YOU", "SAN")}");
         }
 
         private static List<string> FindDescendants(List<string> orbiters)
@@ -51,25 +43,17 @@ namespace Advent_of_Code_2019
                 count++;
                 descendants.Add(orbiter);
 
-                if (objectsDescendants.ContainsKey(orbiter))
-                    descendants.AddRange(FindDescendants(objectsDescendants[orbiter]));
+                if (objectsHierarchy.ContainsKey(orbiter))
+                    descendants.AddRange(FindDescendants(objectsHierarchy[orbiter]));
             }
 
             return descendants;
         }
 
-        private static int FindFirstCommonAscendant(string descendant1, string descendant2)
+        private static int GetMinimumOrbitalTransfersForTwoObjects(string descendant1, string descendant2)
         {
-            List<string> descendant1Ascendants = objectsFullDescendants
-                .Where(v => v.Value.Contains(descendant1))
-                .Select(w => w.Key).ToList();
-
-            List<string> descendant2Ascendants = objectsFullDescendants
-                .Where(v => v.Value.Contains(descendant2))
-                .Select(w => w.Key).ToList();
-
-            //int desc1count = descendant1Ascendants.Count;
-            //int desc2count = descendant2Ascendants.Count;
+            List<string> descendant1Ascendants = GetAscendants(descendant1);
+            List<string> descendant2Ascendants = GetAscendants(descendant2);
 
             var commonNodes = descendant1Ascendants.Intersect(descendant2Ascendants);
             var finalNode = commonNodes
@@ -78,5 +62,10 @@ namespace Advent_of_Code_2019
 
             return (descendant1Ascendants.Count - commonNodes.Count()) + (descendant2Ascendants.Count - commonNodes.Count());
         }
+
+        private static List<string> GetAscendants(string descendant) =>
+            objectsFullDescendants
+                .Where(ascendant => ascendant.Value.Contains(descendant))
+                .Select(ascendant => ascendant.Key).ToList();
     }
 }
