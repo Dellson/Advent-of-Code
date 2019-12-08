8de7e09ec1a3f0666b2e91178d4b0c33682f4663
Paweł Dela <pwdela@gmail.com>
2019-12-08 12:12:47 +0100

    Day 8 (2*)

--- Advent-of-Code-2019/Day-08.cs
+++ Advent-of-Code-2019/Day-08.cs
@@ -7,22 +7,52 @@ namespace Advent_of_Code_2019
 {
     class Day_08
     {
-        static List<List<List<int>>> layers = new List<List<List<int>>>();
-        static List<string> oneDimLayers = new List<string>();
+        private static List<string> oneDimLayers = new List<string>();
         const int maxWidth = 25;
         const int maxHeight = 6;
 
+        static Day_08()
+        {
+            string inputData = File.ReadAllLines(Program.InputFolderPath + "Day-08-input.txt")[0];
+
+            for (int i = 0; i < inputData.Length; i += maxHeight * maxWidth)
+                oneDimLayers.Add(inputData.Substring(i, maxHeight * maxWidth));
+        }
+
         public static void Puzzle()
         {
-            var inputData = File.ReadAllLines(Program.InputFolderPath + "Day-08-input.txt")[0];
+            Func<string, char, int> digitCounter = (layer, target) => layer.Count(c => c == target);            
+            int maxval = oneDimLayers.Min(layer => digitCounter(layer, '0'));
+            string matchingLayer = oneDimLayers.Find(layer => digitCounter(layer, '0') == maxval);
 
-            for (int i = 0; i < inputData.Length; i+= (maxHeight * maxWidth))
-                oneDimLayers.Add(inputData.Substring(i, (maxHeight * maxWidth)));
+            Console.WriteLine($"Puzzle one answer: {digitCounter(matchingLayer, '1') * digitCounter(matchingLayer, '2')}");
+            Console.WriteLine($"\nPuzzle two answer:");
+            
+            string image = FlattenMessage(oneDimLayers);
 
-            var maxval = oneDimLayers.Min(layer => layer.Count(c => c == '0'));
-            var matchingLayer = oneDimLayers.Find(layer => layer.Count(c => c == '0') == maxval);
+            for (int i = 0; i < image.Length; i++)
+            {
+                if (i % maxWidth == 0)
+                    Console.WriteLine();
 
-            Console.WriteLine($"Puzzle one answer: {matchingLayer.Count(l => l == '1') * matchingLayer.Count(l => l == '2')}");
+                Console.Write((image[i] == '1') ? "[]" : "  ");
+            }
+        }
+
+        private static string FlattenMessage(List<string> layers)
+        {
+            char[] destLayer = layers[0].ToCharArray();
+
+            foreach (string layer in layers)
+            {
+                for (int i = 0; i < layer.Length; i++)
+                {
+                    if (destLayer[i] == '2')
+                        destLayer[i] = layer[i];
+                }
+            }
+
+            return new string(destLayer);
         }
     }
 }
