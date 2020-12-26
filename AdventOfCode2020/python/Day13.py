57a43706b94376dbda43a0838cc257ed17e6d71a
Paweł Dela <pawel.dela@amberteam.pl>
2020-12-26 14:08:02 +0100

    2020 day 13 p2 is too slow

--- AdventOfCode2020/python/Day13.py
+++ AdventOfCode2020/python/Day13.py
@@ -2,17 +2,35 @@ import aoc
 
 
 # part 1
-data = [i for i in aoc.input(13)]
-current_time = 6
-earliest_time = int(data[0])
+# data = [i for i in aoc.input(13)]
+# current_time = 6
+# earliest_time = int(data[0])
 
-buses = [int(i) for i in data[1].split(',') if i != 'x']
+# buses = [int(i) for i in data[1].split(',') if i != 'x']
+
+# while True:
+#   bus = [b for b in buses if current_time % b == 0]
+
+#   if len(bus) > 0 and current_time >= earliest_time:
+#     result = bus[0] * abs(earliest_time - current_time)
+#     print(f'part 1: {result}')
+#     break
+#   current_time += 1
+
+# part 2
+data = [i for i in aoc.input(13)]
+buses = [0 if i == 'x' else int(i) for i in data[1].split(',')]
+
+t = 0
+z = lambda x: t % x == 0
 
 while True:
-  bus = [b for b in buses if current_time % b == 0]
-
-  if len(bus) > 0 and current_time >= earliest_time:
-    result = bus[0] * abs(earliest_time - current_time)
-    print(f'part 1: {result}')
+  are_all_valid = True
+  for i in range(len(buses)):
+    if buses[i] != 0 and (t + i) % buses[i] != 0:
+      are_all_valid = False
+      break
+  if are_all_valid:
+    print(t)
     break
-  current_time += 1
+  t += 1
--- AdventOfCode2020/python/aoc.py
+++ AdventOfCode2020/python/aoc.py
@@ -7,3 +7,14 @@ def input(day: int):
     returns: string[] containing input
     """
     return open(f'{os.getcwd()}\AdventOfCode2020\python\input\Day{str(day).zfill(2)}.txt')
+
+def gcd(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+
+    for i in range(1, smaller + 1):
+        if ((x % i == 0) and (y % i == 0)):
+            gcd = i
+    return gcd
