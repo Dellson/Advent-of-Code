4eec2e79de8b32b0a014c6c42a51fbe99357e13e
Paweł Dela <pawel.dela@amberteam.pl>
2021-12-03 20:03:12 +0100

    AoC 2021 Day 1**

--- Advent-of-Code-2021/Day01.cs
+++ Advent-of-Code-2021/Day01.cs
@@ -5,11 +5,11 @@ namespace AdventOfCode2021
 {
     class Day01
     {
-        static List<int> Input;
+        static List<int> _input;
 
         public static void Puzzle()
         {
-            Input = Program.GetNumberInputData("1");
+            _input = Program.GetNumberInputData("1");
 
             Puzzle1();
             Puzzle2();
@@ -17,12 +17,29 @@ namespace AdventOfCode2021
 
         private static void Puzzle1()
         {
-            
+            int count = 0;
+
+            for (int i = 0; i < _input.Count - 1; i++)
+            {
+                if (_input[i + 1] > _input[i]) count++;
+            }
+
+            Console.WriteLine($"Puzzle 1 answer: {count}");
         }
 
         private static void Puzzle2()
         {
-            
+            int count = 0;
+            int previousWindow = _input[0] + _input[1] + _input[2];
+
+            for (int i = 1; i < _input.Count - 2; i++)
+            {
+                int currentWindow = _input[i] + _input[i + 1] + _input[i + 2];
+                if (currentWindow > previousWindow) count++;
+                previousWindow = currentWindow;
+            }
+
+            Console.WriteLine($"Puzzle 2 answer: {count}");
         }
     }
 }
