b0487c3254ce23b7acdc8772c24af1f75c843249
Paweł Dela <pawel.dela@amberteam.pl>
2022-12-02 10:30:41 +0100

    AoC 2022 day 1 **

---
+++ AdventOfCode2022/Day01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022
+{
+    class Day01
+    {
+        static string[] _input;
+
+        public static void Puzzle()
+        {
+            _input = Program.GetTextInputData("1");
+
+            Puzzle(1);
+            Puzzle(3);
+        }
+
+        private static void Puzzle(int takeCount)
+        {
+            List<int> elves = new List<int> { 0 };
+            foreach (var item in _input)
+            {
+                if (item == string.Empty)
+                    elves.Add(0);
+                else
+                    elves[^1] += Convert.ToInt32(item);
+            }
+
+            Console.WriteLine($"Puzzle answer: {elves.OrderByDescending(num => num).Take(takeCount).Sum()}");
+        }
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using AdventOfCode;
+
+namespace AdventOfCode2022
+{
+    class Program : BaseProgram
+    {
+        static Program()
+        {
+            InputFolderPath = Directory.GetCurrentDirectory();
+            InputFolderPath = Path.Combine(InputFolderPath, "Input");
+        }
+
+        static void Main(string[] args)
+        {
+            Day01.Puzzle();
+            Console.ReadKey();
+        }
+    }
+}
